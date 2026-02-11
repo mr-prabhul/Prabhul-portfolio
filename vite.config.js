a3ae955ecbf1a00919176afc2b5e551620fe637b
mr-prabhul <ichayanprabhul2724@gmail.com>
2026-02-12 00:44:44 +0530

    Fix deployment issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base:"/Prabhul-portfolio",
+  base:"/",
   server: {
     allowedHosts: [
       '.trycloudflare.com'
